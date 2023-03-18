b1a27d9a4fae70b0282b5ad3527e217c6685a9b6
irfan rasheed <irfanrasheed273@gmail.com>
2023-03-18 19:47:28 +0530

    Face image collection

---
+++ Face Recognition/collect_images.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import os
+
+name = input("Enter name: ")
+
+# Reading videos
+capture = cv.VideoCapture(0)  # 0 denote primary cam. 1 denote secondary cam
+# capture2 = cv.VideoCapture(1)
+i=0
+
+# Get the current working directory
+cwd = os.getcwd()
+print("Current working directory:", cwd)
+
+# Create a new directory for the images, if it doesn't already exist
+images_dir = os.path.join(cwd, "faces", name)
+if not os.path.exists(images_dir):
+    os.makedirs(images_dir)
+else:
+    # Get the largest image number in the directory
+    filenames = os.listdir(images_dir)
+    filenames = [f for f in filenames if f.endswith(".png")]
+    if filenames:
+        largest_num = max([int(f[:-4]) for f in filenames])
+        i = largest_num + 1
+
+while True:
+    isTrue, frame = capture.read()
+    frame1 = frame.copy()
+    frame_with_text = cv.putText(frame, str(i), (100, 100), cv.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2, cv.LINE_4)
+    cv.imshow('Frame' , frame_with_text)
+
+    if cv.waitKey(10) & 0xFF == ord('d'):
+        break
+    if cv.waitKey(1) & 0xFF == ord(' '):
+        filename = os.path.join(images_dir, f"{i}.png")
+        cv.imwrite(filename, frame1, [cv.IMWRITE_JPEG_QUALITY, 100])
+        i += 1
+
+# capture2.release()
+capture.release()
+cv.destroyAllWindows()
