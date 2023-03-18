cd7f174924c1a3895553101d1301a58d3c3c0cf7
Jayakanth Arun <jayakantharun03@gmail.com>
2023-03-19 01:49:40 +0530

    Calculated Time

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,4 +1,6 @@
-double sample1_number , given_voltage , reference_value1 , voltage_number , sample1 = 0 , voltage , actual_voltage , reference_value2 , current_number , sample2=0 , current , actual_current , current_val;
+
+float sample1_number, given_voltage, reference_value1, voltage_number, sample1 = 0, voltage, actual_voltage, reference_value2, current_number, sample2 = 0, current, actual_current, current_val, power;
+long time, timeinitial, timefinal;
 
 void setup() {
 
@@ -9,63 +11,102 @@ void setup() {
   reference_value1 = given_voltage / sample1_number;
 
   reference_value2 = 5000.0 / 1024.0;
-  
-  for (int i=0;i<200;i++){
-    voltage_number = analogRead(A2);
-    sample1 = sample1 + voltage_number;
 
-    current_number = analogRead(A3);
-    sample2 = sample2 + current_number;
-    
-    delay(2);
-  }
-  
-  sample1 = sample1/200; 
-  voltage = reference_value1 * sample1 / 1000;
-  actual_voltage = voltage * 2;
+  // for (int i=0;i<200;i++){
+  //   voltage_number = analogRead(A2);
+  //   sample1 = sample1 + voltage_number;
 
-  sample2 = sample2 / 200;
-  current = reference_value2 * sample2;
-  current_val = (current/1000) - 2.5;
-  actual_current = current_val * 10;
+  //   current_number = analogRead(A3);
+  //   sample2 = sample2 + current_number;
 
-  Serial.print("voltage : ");
-  Serial.println(actual_voltage);
+  //   delay(2);
+  // }
 
-  Serial.print("current : ");
-  Serial.println(actual_current);
+  // sample1 = sample1/200;
+  // voltage = reference_value1 * sample1 / 1000;
+  // actual_voltage = voltage * 2;
 
-}  
+  // sample2 = sample2 / 200;
+  // current = reference_value2 * sample2;
+  // current_val = (current/1000) - 2.5;
+  // actual_current = abs(current_val * 10);
+
+  // power=actual_voltage*actual_current;
+
+  // Serial.print("voltage : ");
+  // Serial.println(actual_voltage);
+
+  // Serial.print("current : ");
+  // Serial.println(actual_current);
+}
 
 void loop() {
-  sample1=0;
-  sample2=0;
 
-  reference_value2 = 5.0 / 1024.0;
+  sample1 = 0;
+  sample2 = 0;
 
-  for (int i=0;i<200;i++){
+
+  timeinitial = millis();
+  for (int i = 0; i < 200; i++) {
     voltage_number = analogRead(A2);
     sample1 = sample1 + voltage_number;
 
     current_number = analogRead(A3);
     sample2 = sample2 + current_number;
-  
+
     delay(2);
   }
- 
-  sample1 = sample1/200; 
+
+  sample1 = sample1 / 200;
   voltage = reference_value1 * sample1 / 1000;
   actual_voltage = voltage * 2;
 
   sample2 = sample2 / 200;
   current = reference_value2 * sample2;
-  current_val = current - 2.5;
-  actual_current = current_val * 10; 
+  current_val = (current / 1000) - 2.5;
+  actual_current = abs(current_val * 10);
+
+  power = actual_voltage * actual_current;
+
+  timefinal = millis();
+  time = timefinal - timeinitial;
+
+
 
   Serial.print("voltage : ");
   Serial.println(actual_voltage);
-  
+
   Serial.print("current : ");
   Serial.println(actual_current);
+}
+}
+// sample1 = 0;
+// sample2 = 0;
 
+// reference_value2 = 5.0 / 1024.0;
+
+// for (int i = 0; i < 200; i++) {
+//   voltage_number = analogRead(A2);
+//   sample1 = sample1 + voltage_number;
+
+//   current_number = analogRead(A3);
+//   sample2 = sample2 + current_number;
+
+//   delay(2);
+// }
+
+// sample1 = sample1 / 200;
+// voltage = reference_value1 * sample1 / 1000;
+// actual_voltage = voltage * 2;
+
+// sample2 = sample2 / 200;
+// current = reference_value2 * sample2;
+// current_val = current - 2.5;
+// actual_current = current_val * 10;
+
+// Serial.print("voltage : ");
+// Serial.println(actual_voltage);
+
+// Serial.print("current : ");
+// Serial.println(actual_current);
 }
